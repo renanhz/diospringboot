40460ed96b2000a6e463592d4bf4e73210a9e957
Renan Santos <renan.stos077@gmail.com>
2021-08-17 17:41:16 -0300

    Empresa Controller basic CRUD methods

--- src/main/java/com/dio/live/controller/EmpresaController.java
+++ src/main/java/com/dio/live/controller/EmpresaController.java
@@ -1,2 +1,53 @@
-package com.dio.live.controller;public class EmpresaController {
+package com.dio.live.controller;
+
+import com.dio.live.model.Empresa;
+import com.dio.live.model.JornadaTrabalho;
+import com.dio.live.service.EmpresaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+
+@RestController
+@RequestMapping("/empresa")
+public class EmpresaController {
+    @Autowired
+    EmpresaService empresaService;
+
+    @PostMapping
+    public Empresa createEmpresa(@RequestBody Empresa empresa) {
+        return empresaService.createEmpresa(empresa);
+    }
+
+    @GetMapping
+    public List<Empresa> getAllEmpresas() {
+        return empresaService.getAll();
+    }
+
+    @GetMapping("/{empresaId}")
+    public ResponseEntity<Empresa> getEmpresaById(@PathVariable("empresaId") Long empresaId) throws Exception{
+        return ResponseEntity.ok(
+                empresaService.getEmpresaById(empresaId).orElseThrow(
+                        () -> new NoSuchElementException("Empresa Not Found")
+                ));
+    }
+
+    @PutMapping
+    public Empresa updateEmpresa(Empresa empresa) {
+        return empresaService.updateEmpresa(empresa);
+    }
+
+    @DeleteMapping("/{empresaId}")
+    public ResponseEntity deleteEmpresaById(@PathVariable("empresaId") Long empresaId) {
+        try {
+            empresaService.deleteEmpresaById(empresaId);
+        } catch(Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+        return (ResponseEntity<Empresa>) ResponseEntity.ok();
+    }
+
 }
--- src/main/java/com/dio/live/service/EmpresaService.java
+++ src/main/java/com/dio/live/service/EmpresaService.java
@@ -1,2 +1,36 @@
-package com.dio.live.service;public class EmpresaService {
+package com.dio.live.service;
+
+import com.dio.live.model.Empresa;
+import com.dio.live.repository.EmpresaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class EmpresaService {
+
+    @Autowired
+    EmpresaRepository empresaRepository;
+
+    public Empresa createEmpresa(Empresa empresa) {
+        return empresaRepository.save(empresa);
+    }
+
+    public List<Empresa> getAll() {
+        return empresaRepository.findAll();
+    }
+
+    public Optional<Empresa> getEmpresaById(Long empresaId) {
+        return empresaRepository.findById(empresaId);
+    }
+
+    public Empresa updateEmpresa(Empresa empresa) {
+        return empresaRepository.save(empresa);
+    }
+
+    public void deleteEmpresaById(Long empresaId) {
+        empresaRepository.deleteById(empresaId);
+    }
 }
